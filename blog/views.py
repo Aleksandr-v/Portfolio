c3e0264ab95517d393eb4b1895ac8d15390b6f57
Aleksandr-v <mixft@mail.ru>
2018-10-05 22:59:03 +1100

    First Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from . models import Post
 
-# Create your views here.
+def index(request):
+    post = get_object_or_404(Post, id=1)
+    return render(request, 'blog/post/index.html', {'post':post})
